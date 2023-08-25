7358eeb2a0cf22670be5d4508742c6c93ed189d2
TatianaProzorova <2tatiana.prozorova@gmail.com>
2023-08-25 20:42:49 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public Product(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{Name} {Price}");
+        }
+    }
+
+    public class Store
+    {
+        public List<Product> Products;
+        public Store()
+        {
+            Products = new List<Product>
+            {
+                new Product("Хлеб", 25),
+                new Product("Молоко", 100),
+                new Product("Печенье", 50),
+                new Product("Масло", 250),
+                new Product("Йогурт", 300),
+                new Product("Сок", 80)
+            };
+        }
+
+        public void ShowCatalog()
+        {
+            foreach (Product product in Products)
+            {
+                product.Print();
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Store onlineStore = new Store();
+
+            Console.WriteLine("Здравствуйте. Выберите действие:");
+            Console.WriteLine("1. Показать каталог продуктов");
+            Console.WriteLine("Выберите номер действия, которое хотите совершить.");
+            int numberAction = Convert.ToInt32(Console.ReadLine());
+
+            switch (numberAction)
+            {
+                case 1: onlineStore.ShowCatalog(); break;
+                default: Console.WriteLine("Выберите номер действия из списка"); break;
+            }
+        }
+    }
+}
