671f882d45d0d07bff6dfbf7ba4b506032854781
TatianaProzorova <2tatiana.prozorova@gmail.com>
2023-08-25 21:16:57 +0300

    move classes to separate cs files

---
+++ Order.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Store
+{
+    public class Order
+    {
+        public List<Product> Products;
+        public decimal FullPrice;
+        public Order(List<Product> products)
+        {
+            Products = products;
+
+            foreach (var product in products)
+            {
+                FullPrice += product.Price;
+            }
+        }
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Store
+{
+    public class Product
+    {
+        public string Name;
+        public decimal Price;
+        public Product(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"{Name} {Price}");
+        }
+    }
+}
